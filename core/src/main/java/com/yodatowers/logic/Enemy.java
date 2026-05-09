e6ca572f0ece4811ac72e151e0eaf6d1e691ef76
abmacatangay <abmacatangay4@up.edu.ph>
2026-05-09 15:25:20 +0800

    created logic package and base enemy class to be implemented

---
+++ core/src/main/java/com/yodatowers/logic/Enemy.java
@@ -0,0 +1,58 @@
+package com.yodatowers.logic;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+
+public abstract class Enemy {
+    protected Sprite sprite; // Visual
+    protected Rectangle bounds; //Hitbox (Colliusion)
+    protected float speed; // Movement units/seconds
+    protected int health; // Hit points
+
+    // Enemy Properties
+    public Enemy(Texture texture, float x, float y, float width, float height, float speed, int health) {
+        this.sprite = new Sprite(texture);
+        this.sprite.setSize(width, height);
+        this.sprite.setPosition(x, y);
+        this.speed = speed;
+        this.health = health;
+        // Bounds should match sprite's dimensions
+        this.bounds = new Rectangle(x, y, width, height);
+    }
+
+    public void update(float delta, float targetX, float targetY) {
+        // Calculate x and y distance to the target
+        float xMovement = targetX - sprite.getX();
+        float yMovement = targetY - sprite.getY();
+        float len = (float)Math.sqrt(xMovement * xMovement + yMovement * yMovement);
+
+        // Normalize vector so enemy moves at the same speed regardless of distance
+        if (len != 0) {
+            xMovement /= len;
+            yMovement /= len;
+        }
+
+        // Move sprite based on direction and time
+        sprite.translate(xMovement * speed * delta, yMovement * speed * delta);
+        // Hitbox should update as sprites position updates also
+        bounds.set(sprite.getX(), sprite.getY(), sprite.getWidth(), sprite.getHeight());
+    }
+
+    public void draw(SpriteBatch batch) {
+        sprite.draw(batch);
+    }
+
+    public Rectangle getBounds() {
+        return bounds;
+    }
+
+    public void takeDamage(int amount) {
+        health -= amount;
+    }
+
+    public boolean isDead() {
+        return health <= 0;
+    }
+}
